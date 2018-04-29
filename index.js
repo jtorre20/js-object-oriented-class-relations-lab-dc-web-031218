891696f779ef047852bfe87ee9057cdf16f7bbf2
jtorre20 <jtorres1989@gmail.com>
2018-04-29 18:07:09 -0400

    created driver class

---
+++ index.js
@@ -0,0 +1,11 @@
+let store = { drivers: [], passengers: [] };
+let driverId = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverId;
+    this.name = name;
+    
+    store.drivers.push(this);
+  }
+}
